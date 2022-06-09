65a78e52becf4b356bd7d8d5e633c3ea3ca6a8d9
rohit-gupta09 <gupta2002rohit@gmail.com>
2022-06-09 23:38:26 +0530

    next permutation question

---
+++ Question15.java
@@ -0,0 +1,27 @@
+// next permutation 
+
+package lovebabber;
+import java.util.*;
+import java.io.*;
+public class Question15 {
+	
+	static void nextPermutation(int arr[])
+	{t1r
+		for()
+	}
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int n=sc.nextInt();
+		int arr[]=new int[n];
+		for(int i=1;i<n;i++)
+		{
+			arr[i]=sc.nextInt();
+		}
+		
+		nextPermutation(arr);
+
+	}
+
+}
