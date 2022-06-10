6418cded34a5b0964836ee4d129c57e040352e24
rohit-gupta09 <gupta2002rohit@gmail.com>
2022-06-10 21:28:39 +0530

    Final Solution of next permuation problem

--- Question15.java
+++ Question15.java
@@ -5,14 +5,46 @@ import java.util.*;
 import java.io.*;
 public class Question15 {
 	
-	static void nextPermutation(int arr[])
-	{t1r
-		for()
+	static void nextPermutation(int nums[])
+	{
+		int i=nums.length-2;
+        while(i>=0 && nums[i]>=nums[i+1])
+        {
+            i--;
+        }
+        if(i>=0)
+        {
+            int j=nums.length-1;
+            while(nums[j]<=nums[i])
+            {
+                j--;
+            }
+            swap(nums,i,j);
+        }
+        reverse(nums,i+1);
+	}
+	
+	public static void swap(int nums[],int i,int j)
+	{
+		int temp=nums[i];
+		nums[i]=nums[j];
+		nums[j]=temp;
+	}
+	
+	public static void reverse(int nums[],int i)
+	{
+		int j=i;
+		int n=nums.length-1;
+		while(j<n)
+		{
+			swap(nums,j,n);
+			j++;
+			n--;
+		}
 	}
 
 	public static void main(String[] args) {
 		Scanner sc=new Scanner(System.in);
-		int n=sc.nextInt();
 		int n=sc.nextInt();
 		int arr[]=new int[n];
 		for(int i=1;i<n;i++)
